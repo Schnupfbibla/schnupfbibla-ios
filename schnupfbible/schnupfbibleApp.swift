ad5e2198ae0397d358660d20248be42f75e8ab19
Jesse Born <jesse.born@gmx.ch>
2023-01-19 18:26:35 +0100

    Initial Commit

---
+++ schnupfbible/schnupfbibleApp.swift
@@ -0,0 +1,17 @@
+//
+//  schnupfbibleApp.swift
+//  schnupfbible
+//
+//  Created by Jesse Born on 19.01.23.
+//
+
+import SwiftUI
+
+@main
+struct schnupfbibleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
